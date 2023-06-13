005b9e8e342a528fa546f8dd1291f8f3dde42bfc
EmranAli-15 <mohammademranali15@gmail.com>
2023-06-13 20:10:01 +0600

    displayed all tasks

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import AllTasks from './allTasks/AllTasks';
 
 const Home = () => {
+    
     return (
         <div>
-            <h3>This is home page</h3>
+            <div className="grid grid-cols-12 gap-4">
+                <div className="col-start-1 col-end-4">01</div>
+                <div className="col-start-4 col-end-10">
+                    <AllTasks></AllTasks>
+                </div>
+                <div className="col-start-10 col-end-13">03</div>
+            </div>
         </div>
     );
 };
---
+++ src/pages/home/allTasks/AllTasks.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const AllTasks = () => {
+    const [tasks, setTasks] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/allTasks')
+            .then(res => res.json())
+            .then(data => setTasks(data))
+    }, [])
+
+    return (
+        <div>
+            {
+                tasks.map(task =>
+                    <div key={task._id} className="card my-4 card-compact w-full bg-base-100 shadow-xl">
+                        <div className="card-body">
+                            <h2 className="md:text-2xl font-bold p-2">{task.title}</h2>
+                            <div className='text-lg'>
+                                {
+                                    task.details.length < 200 ? task.details :
+                                        <div>{task.details.slice(0, 200)} <br />
+                                            <p className='text-end'><Link>read more</Link>
+                                            </p>
+                                        </div>
+                                }
+                            </div>
+                        </div>
+                    </div>)
+            }
+        </div>
+    );
+};
+
+export default AllTasks;
